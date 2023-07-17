204c12c70515d6c1a5151c6b382b4b3f92242c8f
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2023-07-17 13:18:19 +0100

    App .js Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar/Navbar';
+import Feed from './components/Feed/Feed';
+import VideoInfo from './components/VideoInfo/VideoInfo';
+import ChannelInfo from './components/ChannelInfo/ChannelInfo';
+import SearchFeed from './components/SearchFeed/SearchFeed';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" elememt={<VideoInfo />} />
+          <Route path="/channel/:id" elememt={<ChannelInfo />} />
+          <Route path="/search/:searchTerm" elememt={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
